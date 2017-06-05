a2c970e64daf2d7397fac1297a06a2c50ae699f8
Aimee Borda <bordaa@scss.tcd.ie>
2017-06-05 06:43:04 +0100

    set up template for week 3 assignment

---
+++ src/main/java/week3/WordSearch.java
@@ -0,0 +1,26 @@
+package week3;
+
+
+public class WordSearch {
+
+  /*
+   * Given a grid of letters and a dictionary, find all the words from the dictionary that can be
+   * formed in the grid.The rules for forming a word:
+   *    ● You can start at any position.
+   *    ● You can move to one of the 8 adjacent cells (horizontally/vertically/diagonally).
+   *    ● You can't visit the same cell twice in the same word.Assume you have a dictionary class
+   *      with these two methods:
+   *    ● isWord(string): Returns whether the given string is a valid word.
+   *    ● isPrefix(string): Returns whether the given string is a prefix of at least one word in
+   *      the dictionary.
+   *
+   * Your function receives the number of rows, number of columns, a 2-dimensional array of
+   * characters (of the native char data type), and the dictionary. You should return the set of all
+   * words found.
+   */
+
+
+  public String[] findWords(char[][] grid, String[] dictionary) {
+    return null;
+  }
+}
---
+++ src/main/java/week3/WordSearchTest.java
@@ -0,0 +1,38 @@
+package week3;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class WordSearchTest {
+
+  @Before
+  public void setUp() {
+
+  }
+
+  @Test
+  public void testEmpty() {
+
+  }
+
+  @Test
+  public void testSimpleSuccessful() {
+
+  }
+
+  @Test
+  public void testSimpleEdgeCases() {
+
+  }
+
+  @Test
+  public void testSimpleNegative() {
+
+  }
+
+  @Test
+  public void testSimpleDuplicates() {
+
+  }
+}
