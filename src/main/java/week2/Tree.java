09c1ec5148e812c600abb6b665e4479c3d6b4e42
Aimee Borda <bordaa@scss.tcd.ie>
2017-06-06 20:10:55 +0100

    reverted to old algorithm and removed global boolean by creating an inner class Result

--- src/main/java/week2/Tree.java
+++ src/main/java/week2/Tree.java
@@ -1,68 +1,98 @@
 package week2;
 
-
-import java.util.ArrayList;
-import java.util.Optional;
-
 class Tree<T> {
 
   /*
    * Question 1: Given a Binary Tree and a key, write a function that prints all the ancestors
    *              of the key in the given binary tree.
-   *              Example: For the key 5 and the following tree we should print: 5, 3, 9, 16.
+   *              Example: For the key 5 and the following tree we should print: 3, 9, 16.
    *
    *  Overview: We define a private method to recursively parse through the tree searching for the
    *           element. Once the value is found, the method returns true to signal up to call stack
-   *            that element is found and the ancestors are all added to an arraylist which in the
-   *            end is the resulting string
+   *            that element is found.
    *
    *  Time: O(N) where N is the number of elements in tree
    */
   String printAncestors(Node<T> root, T key) {
-    ArrayList<T> ancestors = new ArrayList<>();
-    findAncestors(key, root, ancestors);
+    StringBuilder sb = new StringBuilder();
+    printAncestors(key, root, sb);
 
-    return ancestors.toString();
+    return sb.toString();
   }
 
-  private boolean findAncestors(T key, Node<T> element, ArrayList<T> ancestors) {
+  private boolean printAncestors(T key, Node<T> element, StringBuilder sb) {
     if (element == null) {
       return false;
     }
 
-    if (element.getKey().equals(key)
-        || findAncestors(key, element.getLeft(), ancestors)
-        || findAncestors(key, element.getRight(), ancestors)) {
-
-      // signals back that element is found and hence the recursion path back are the ancestors
-      ancestors.add(element.getKey());
+    if (element.getKey().equals(key)) {
+      //signal back that element is found and hence the recursion path back are the ancestors
       return true;
+    } else if (printAncestors(key, element.getLeft(), sb) || printAncestors(key,
+        element.getRight(), sb)) {
+      sb.append(sb.length() > 0 ? ", " : "");
+      sb.append(element.getKey().toString());
+
+      return true;
+    } else {
+      return false;
     }
 
-    return false;
   }
-
 
   /* Question 2: Design an algorithm and write code to find the lowest common ancestor of two nodes
    *             in a binary tree. Avoid storing additional nodes in a data structure
    *
-   * Overview: The solution uses findAncestors method to get the ancestor of both keys and then
-   *            return the first common element in the lists
+   * Overview: The solution is implemented recursively.
    *
+   *           Possible alternatives (omitted duals) considered in the recursive commonAncestor method:
+   *           1) None of the element are in the tree (else branch in commonAncestor)
+   *           2) One of the element is in the tree (bothFound is not set to true)
+   *           3) key1 is an ancestor of key2 => key2 (else if branch)
+   *           4) key1 is descendant on the left of some node and key2 is a descendant on thr right of the same node (if branch)
+   *
+   *  Time: O(N) where N is the number of elements in tree
    */
-  Optional<T> commonAncestor(Node<T> root, T key1, T key2) {
+  T commonAncestor(Node<T> root, T key1, T key2) {
+    Result res = commonAncestor(key1, key2, root);
 
-    ArrayList<T> key1Ancestor = new ArrayList<>();
-    ArrayList<T> key2Ancestor = new ArrayList<>();
+    return res != null && res.isAncestor ? res.key : null;
+  }
 
-    if (findAncestors(key1, root, key1Ancestor) && findAncestors(key2, root, key2Ancestor)) {
+  private Result commonAncestor(T key1, T key2, Node<T> element) {
 
-      key1Ancestor.retainAll(key2Ancestor);
-
-      return key1Ancestor.stream().findFirst();
+    if (element == null) {
+      return null;
     }
 
-    return Optional.empty();
+    Result resLeft = commonAncestor(key1, key2, element.getLeft());
+    Result resRight = commonAncestor(key1, key2, element.getRight());
+
+    if (resLeft != null && resRight != null) {
+      return new Result(element.getKey());
+    } else if (element.getKey().equals(key1) || element.getKey().equals(key2)) {
+      boolean isAncestor = (resLeft != null || resRight != null || key1 == key2);
+      return new Result(element.getKey(), isAncestor);
+    } else {
+      return resLeft != null ? resLeft : resRight;
+    }
+  }
+
+
+  private class Result {
+
+    boolean isAncestor;
+    T key;
+
+    public Result(T key) {
+      isAncestor = true;
+      this.key = key;
+    }
+
+    public Result(T key, boolean isAncestor) {
+      this.isAncestor = isAncestor;
+      this.key = key;
+    }
   }
 
 }
