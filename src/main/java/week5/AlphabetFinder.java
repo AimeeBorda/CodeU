61b108f1382be71611866174c8b7bc802ae83831
Aimee Borda <bordaa@scss.tcd.ie>
2017-07-04 19:56:22 +0100

    working solution but needs more test cases

--- src/main/java/week5/AlphabetFinder.java
+++ src/main/java/week5/AlphabetFinder.java
@@ -1,56 +1,118 @@
 package week5;
 
 
+import static java.util.stream.Collectors.toList;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class AlphabetFinder {
 
+  /*
+   * We first get all the sets of ordering from the words (in ordering)
+   * e.g. [ART, RAT, CAR, CAT] -> [[A,R,C],[R,T]]  (corrected to include single letters)
+   *
+   * we then call getAlphabet to infer alphabet from grouping
+   *
+   * Time Complexity: O(
+   * Space Complexity: O(
+   */
   public static List<Character> findAlphabet(String[] words) {
 
     if (words == null || words.length == 0) {
-      return new LinkedList<>();
+      return new ArrayList<>();
     }
 
-    List<List<String>> suffixes = new LinkedList<>();
-    List<List<Character>> ordering = new LinkedList<>();
+    //get list of all ordering from words
+    List<List<Character>> ordering = new ArrayList<>();
+    Queue<List<String>> queue = new LinkedList<>();
 
-    suffixes.add(Arrays.asList(words));
+    //start with all
+    queue.add(Arrays.asList(words));
 
-    while (!suffixes.isEmpty()) {
-      LinkedList<Character> tempOrdering = new LinkedList<>();
-      LinkedList<String> tempSuffix = new LinkedList<>();
+    while (!queue.isEmpty()) {
+      List<String> suffixes = queue.poll();
 
-      for (String s : suffixes.remove(0)) {
-        if (!tempOrdering.contains(s.charAt(0))) {
-          tempOrdering.add(s.charAt(0));
-        }
+      ordering.add(getOrdering(suffixes));
+      queue.addAll(getSetsOfSuffixes(suffixes));
 
-        if (s.length() > 1) {
-          tempSuffix.add(s.substring(1));
-        }
-      }
-
-      tempSuffix.removeIf(s -> s.length() <= 1);
-      if (tempSuffix.size() > 1) {
-        suffixes.add(tempSuffix);
-      }
-
-      if (tempOrdering.size() > 1) {
-        ordering.add(tempOrdering);
-      }
+      queue.removeIf(q -> q.isEmpty());
     }
 
     return getAlphabet(ordering);
   }
 
+  private static List<Character> getOrdering(List<String> suffixes) {
+    return suffixes
+        .stream()
+        .filter(s -> s.length() > 0)
+        .map(s -> s.charAt(0))
+        .distinct()
+        .collect(toList());
+  }
+
+  private static Collection<List<String>> getSetsOfSuffixes(List<String> suffixes) {
+    Collection<List<String>> values = suffixes
+        .stream()
+        .filter(s -> s.length() > 1)
+        .collect(Collectors.groupingBy(s -> s.charAt(0)))
+        .values();
+
+    values.forEach(v -> v.replaceAll(s -> s.substring(1)));
+
+    return values;
+  }
+
   private static List<Character> getAlphabet(List<List<Character>> ordering) {
-    List<Character> alphabet = new LinkedList<>();
-
-    while (!ordering.isEmpty()) {
-
+    if (ordering == null || ordering.isEmpty()) {
+      return new ArrayList<>();
     }
+
+    List<Character> alphabet = new LinkedList<>(ordering.remove(0));
+
+    ordering
+        .stream()
+        .distinct()
+        .forEach(dep -> {
+          for (int i = 0; i < dep.size(); i++) {
+
+            int next = getNextIndex(alphabet, dep, i + 1);
+            int prev = getPreviousIndex(alphabet, dep, i - 1);
+
+            if (next == -1 && prev == -1 && !alphabet.contains(dep.get(i))) {
+              alphabet.add(dep.get(i));
+            } else if (next > prev) {
+              alphabet.remove(dep.get(i));
+              alphabet.add(Math.max(prev + 1, next), dep.get(i));
+            }
+          }
+        });
+
     return alphabet;
+  }
+
+  private static int getPreviousIndex(List<Character> alphabet,
+      List<Character> ordering, int index) {
+    if (index >= 0) {
+      return IntStream.range(index, 0)
+          .filter(i -> alphabet.contains(ordering.get(i)))
+          .findFirst().orElse(-1);
+    }
+
+    return -1;
+
+  }
+
+  private static int getNextIndex(List<Character> alphabet,
+      List<Character> ordering, int index) {
+    return IntStream.range(index, ordering.size())
+        .filter(i -> alphabet.contains(ordering.get(i)))
+        .findFirst().orElse(-1);
   }
 }
--- src/main/java/week5/AlphabetFinderTest.java
+++ src/main/java/week5/AlphabetFinderTest.java
@@ -24,20 +24,15 @@ public class AlphabetFinderTest {
 
   @Test
   public void testContradiction() {
-    assertThat(AlphabetFinder.findAlphabet(new String[]{"ART", "RAT", "CAT", "CAR"}),
+    assertThat(AlphabetFinder.findAlphabet(new String[]{"ART", "RAT", "ARC", "CAT", "CAR"}),
         is(Arrays.asList('A', 'T', 'R', 'C')));
   }
 
-  @Test
-  public void testAmbiguous() {
-    assertThat(AlphabetFinder.findAlphabet(new String[]{"ART", "RAT", "CAT", "CAR"}),
-        is(Arrays.asList('A', 'T', 'R', 'C')));
-  }
 
   @Test
   public void testCasing() {
-    assertThat(AlphabetFinder.findAlphabet(new String[]{"ART", "RAT", "CAT", "CAR"}),
-        is(Arrays.asList('A', 'T', 'R', 'C')));
+    assertThat(AlphabetFinder.findAlphabet(new String[]{"ART", "RAT", "cAT", "CAR", "CAa"}),
+        is(Arrays.asList('A', 'R', 'c', 'C', 'T', 'a')));
   }
 
   @Test
@@ -48,7 +43,22 @@ public class AlphabetFinderTest {
 
   @Test
   public void testDuplication() {
-    assertThat(AlphabetFinder.findAlphabet(new String[]{"ART", "RAT", "CAT", "CAR"}),
-        is(Arrays.asList('A', 'T', 'R', 'C')));
+    assertThat(AlphabetFinder.findAlphabet(new String[]{"ART", "RAT", "RAD", "RAT", "CAT", "CAR"}),
+        is(Arrays.asList('A', 'T', 'R', 'C', 'D')));
+  }
+
+  @Test
+  public void testAllLetter() {
+    assertThat(AlphabetFinder.findAlphabet(new String[]{"ART", "RAT", "CAT", "CAR", "CAD"}),
+        is(Arrays.asList('A', 'T', 'R', 'C', 'D')));
+  }
+
+  @Test
+  public void testDiffLen() {
+    //in this example 'D' is before 'S' because 'D' is encountered on the third
+    //character of the word whereas 'S' on the fourth
+    assertThat(
+        AlphabetFinder.findAlphabet(new String[]{"ARTS", "RATTA", "CAT", "CAR", "CADDDDDDDDDD"}),
+        is(Arrays.asList('A', 'T', 'R', 'C', 'D', 'S')));
   }
 }
