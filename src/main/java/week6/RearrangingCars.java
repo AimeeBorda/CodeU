c9a7f20b511a7b592c668b7906204d94a75b27f0
Aimee Borda <bordaa@tcd.ie>
2017-07-17 14:45:08 +0100

    first draft of solution - reverse do not work

--- src/main/java/week6/RearrangingCars.java
+++ src/main/java/week6/RearrangingCars.java
@@ -1,39 +1,89 @@
 package week6;
 
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class RearrangingCars {
 
+  private static final int EMPTY = 0;
+
   /*
+   * First, we create a map of wrongly positioned cars, car no. -> position
+   * Iteratively, replace 0 with the correct car. We remove cars that are in correct pos from map
    *
-   * Time Complexity:
-   * Space Complexity:
+   * If 0 becomes in the correct position but map is not empty, move 0 to first incorrect car pos
+   * and start iterations again until map is empty.
+   *
+   * Time Complexity: O(n)
+   * Space Complexity: O(n)
    * Assumptions:
    *    1) len(source) = len(target)
-   *    2) arrays contain O...n, where n is the length of the array (this means no duplications)
+   *    2) arrays contain O...n, this means no duplications
+   *
+   * where n is the length of the array
    */
-  public static String rearrangeCars(int[] source, int[] target) {
+  public static String rearrangeCars(final int[] source, final int[] target) {
 
     if (source == null || target == null || source.length != target.length || source.length == 0) {
       return "";
     }
 
-    return "";
-  }
+    HashMap<Integer, Integer> cars = new HashMap<>();
+    int emptyBox = -1;
 
-  public String getMoves(Move[] moves) {
-    return Arrays.stream(moves).map(Move::toString).reduce(String::concat).orElse("");
-  }
+    //create map car -> pos and get index of emptyBox
+    for (int i = 0; i < source.length; i++) {
+      if (source[i] != target[i] && source[i] != EMPTY) {
+        cars.put(source[i], i);
+      }
 
-  private class Move {
-
-    int source;
-    int target;
-
-    @Override
-    public String toString() {
-      return String.format("move from %d to %d \n", source, target);
+      if (source[i] == EMPTY) {
+        emptyBox = i;
+      }
     }
+
+    StringBuilder sb = new StringBuilder();
+    while (!cars.isEmpty()) {
+      emptyBox = correctEmpty(cars, target, emptyBox, sb);
+      emptyBox = moveEmpty(cars, target, emptyBox, sb);
+    }
+
+    return sb.toString();
+  }
+
+  private static int correctEmpty(Map<Integer, Integer> cars, int[] target, int emptyBox,
+      StringBuilder sb) {
+    if (target[emptyBox] == EMPTY && !cars.isEmpty()) {
+      Entry<Integer, Integer> wrongPosCar = cars.entrySet().iterator().next();
+      appendMove(emptyBox, wrongPosCar.getValue(), sb);
+      return cars.replace(wrongPosCar.getKey(), emptyBox);
+    }
+
+    return emptyBox;
+  }
+
+  private static int moveEmpty(Map<Integer, Integer> cars, int[] target, int emptyBox,
+      StringBuilder sb) {
+    if (cars.containsKey(target[emptyBox])) {
+      appendMove(emptyBox, cars.get(target[emptyBox]), sb);
+      return cars.remove(target[emptyBox]);
+    }
+
+    return emptyBox;
+  }
+
+  private static void appendMove(int source, int target, StringBuilder sb) {
+    sb.append("move from ");
+    sb.append(source);
+    sb.append(" to ");
+    sb.append(target);
+    sb.append("\n");
+  }
+
+
+  public void printRearrangingCars(int[] source, int[] target) {
+    System.out.print(rearrangeCars(source, target));
   }
 }
--- src/main/java/week6/RearrangingCarsTest.java
+++ src/main/java/week6/RearrangingCarsTest.java
@@ -19,42 +19,60 @@ public class RearrangingCarsTest {
 
   @Test
   public void testExample() {
-    String res = "move from 0 to 2\n"
-        + "move from 3 to 0\n"
-        + "move from 1 to 3\n"
-        + "move from 2 to 1\n"
-        + "move from 3 to 2\n";
+    String res =
+        "move from 2 to 1\n"
+            + "move from 1 to 0\n"
+            + "move from 0 to 3\n";
     assertEquals(res, RearrangingCars.rearrangeCars(new int[]{1, 2, 0, 3}, new int[]{3, 1, 2, 0}));
   }
 
   @Test
   public void testReverse() {
-    String res = "move from 0 to 2\n"
-        + "move from 3 to 0\n"
-        + "move from 1 to 3\n"
-        + "move from 2 to 1\n"
-        + "move from 3 to 2\n";
+    String res =
+        "move from 0 to 2\n"
+            + "move from 3 to 0\n"
+            + "move from 1 to 3\n"
+            + "move from 2 to 1\n"
+            + "move from 3 to 2\n";
     assertEquals(res, RearrangingCars.rearrangeCars(IntStream.rangeClosed(0, 10).toArray(),
         IntStream.rangeClosed(10, 0).toArray()));
   }
 
   @Test
+  public void testReverseWithEmptyInMiddle() {
+    String res =
+        "move from 0 to 10\n"
+            + "move from 10 to 1\n"
+            + "move from 1 to 9\n"
+            + "move from 2 to 10\n"
+            + "move from 3 to 2\n";
+    assertEquals(res, RearrangingCars.rearrangeCars(new int[]{1, 2, 3, 4, 5, 0, 6, 7, 8, 9, 10},
+        IntStream.rangeClosed(10, 0).toArray()));
+  }
+
+  @Test
   public void testZeroCorrectlyPlacedBefore() {
-    String res = "move from 0 to 2\n"
-        + "move from 3 to 0\n"
-        + "move from 1 to 3\n"
-        + "move from 2 to 1\n"
-        + "move from 3 to 2\n";
+    String res =
+        "move from 2 to 1\n"
+            + "move from 1 to 0\n"
+            + "move from 0 to 3\n"
+            + "move from 3 to 1\n";
     assertEquals(res, RearrangingCars.rearrangeCars(new int[]{1, 2, 0, 3}, new int[]{3, 0, 2, 1}));
   }
 
   @Test
   public void testZeroCorrectlyPlacedBeforeAndReverse() {
-    String res = "move from 0 to 2\n"
-        + "move from 3 to 0\n"
-        + "move from 1 to 3\n"
-        + "move from 2 to 1\n"
-        + "move from 3 to 2\n";
+    String res =
+        "move from 2 to 1\n"
+            + "move from 1 to 0\n"
+            + "move from 0 to 3\n"
+            + "move from 3 to 1\n"
+            + "move from 1 to 4\n"
+            + "move from 4 to 7\n"
+            + "move from 7 to 1\n"
+            + "move from 1 to 5\n"
+            + "move from 5 to 6\n"
+            + "move from 6 to 1\n";
     assertEquals(res, RearrangingCars
         .rearrangeCars(new int[]{1, 2, 0, 3, 5, 6, 7, 8}, new int[]{3, 0, 2, 1, 8, 7, 6, 5}));
   }
