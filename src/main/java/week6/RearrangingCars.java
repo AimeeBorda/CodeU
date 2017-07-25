b308f6dc799eb78bfbd71f7fd09689719d4a40d3
Aimee Borda <bordaa@scss.tcd.ie>
2017-07-25 21:19:46 +0100

    created Move class and arranged Test accordingly

--- src/main/java/week6/RearrangingCars.java
+++ src/main/java/week6/RearrangingCars.java
@@ -6,10 +6,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.logging.Logger;
 
 public class RearrangingCars {
 
   private static final int EMPTY = 0;
+  private static final Logger logger = Logger.getLogger(RearrangingCars.class.getName());
 
   /*
    * First, we create a map of wrongly positioned cars, car no. -> position
@@ -26,37 +28,32 @@ public class RearrangingCars {
    *
    * where n is the length of the array
    */
-  public static List<Integer> pathForEmpty(final int[] source, final int[] target) {
+  public static List<Move> rearrangeCars(final int[] source, final int[] target) {
+
+    List<Move> res = new ArrayList<Move>() {
+      @Override
+      public String toString() {
+        return super.toString().replaceAll("\\[|\\]|(, )", "");
+      }
+    };
 
     if (source == null || target == null || source.length != target.length) {
-      return new ArrayList<>();
+      return res;
     }
 
     //creates map (wrongly positioned) car id -> position and get index of the empty Box
-    HashMap<Integer, Integer> cars = new HashMap<>();
-    int emptyBox = populateMap(source, target, cars);
+    HashMap<Integer, Integer> misplacedCars = new HashMap<>();
+    int emptyBox = populateMap(source, target, misplacedCars);
 
-    List<Integer> res = new ArrayList<>(cars.size());
-    res.add(emptyBox);
-    while (!cars.isEmpty()) {
-      emptyBox = correctEmpty(cars, target, emptyBox, res);
-      emptyBox = moveEmpty(cars, target, emptyBox, res);
+    while (!misplacedCars.isEmpty()) {
+      //if emptyBox is in right pos, we swap the emptyBox with one of the wrong cars
+      emptyBox = correctEmpty(misplacedCars, target, emptyBox, res);
+      emptyBox = moveEmpty(misplacedCars, target, emptyBox, res);
     }
 
     return res;
   }
 
-
-  public static String rearrangeCars(final int[] source, final int[] target) {
-    List<Integer> res = pathForEmpty(source, target);
-
-    StringBuilder sb = new StringBuilder();
-    for (int i = 1; i < res.size(); i++) {
-      sb.append("move from " + res.get(i - 1) + " to " + res.get(i) + "\n");
-    }
-
-    return sb.toString();
-  }
 
   /*
    * Creates a map of all cars that needs to be moved (we never move a car that is in the right
@@ -69,13 +66,14 @@ public class RearrangingCars {
    *
    * where n is the length of the arrays source and target
    */
-  private static int populateMap(int[] source, int[] target, HashMap<Integer, Integer> cars) {
+  private static int populateMap(int[] source, int[] target,
+      HashMap<Integer, Integer> misplacedCars) {
     int emptyBox = -1;
     for (int i = 0; i < source.length; i++) {
       if (source[i] == EMPTY) {
         emptyBox = i;
       } else if (source[i] != target[i]) {
-        cars.put(source[i], i);
+        misplacedCars.put(source[i], i);
       }
     }
     return emptyBox;
@@ -91,10 +89,10 @@ public class RearrangingCars {
   * Space Complexity: O(1)
   */
   private static int correctEmpty(Map<Integer, Integer> cars, int[] target, int emptyBox,
-      List<Integer> res) {
+      List<Move> moves) {
     if (target[emptyBox] == EMPTY) {
       Entry<Integer, Integer> wrongPosCar = cars.entrySet().iterator().next();
-      res.add(wrongPosCar.getValue());
+      move(emptyBox, wrongPosCar.getValue(), moves);
       return cars.replace(wrongPosCar.getKey(), emptyBox);
     }
 
@@ -109,12 +107,35 @@ public class RearrangingCars {
   * Space Complexity: O(1)
   */
   private static int moveEmpty(Map<Integer, Integer> cars, int[] target, int emptyBox,
-      List<Integer> res) {
+      List<Move> moves) {
     if (cars.containsKey(target[emptyBox])) {
-      res.add(cars.get(target[emptyBox]));
+      move(emptyBox, cars.get(target[emptyBox]), moves);
       return cars.remove(target[emptyBox]);
     }
 
     return emptyBox;
+  }
+
+  private static boolean move(int source, int target, List<Move> moves) {
+    Move m = new Move(source, target);
+    logger.info(m.toString());
+
+    return moves.add(m);
+  }
+
+  private static class Move {
+
+    private final int source;
+    private final int target;
+
+    private Move(int source, int target) {
+      this.source = source;
+      this.target = target;
+    }
+
+    @Override
+    public String toString() {
+      return "move from " + source + " to " + target + "\n";
+    }
   }
 }
--- src/main/java/week6/RearrangingCarsTest.java
+++ src/main/java/week6/RearrangingCarsTest.java
@@ -1,27 +1,21 @@
 package week6;
 
 
-import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
 
-import java.util.Arrays;
 import java.util.stream.IntStream;
 import org.junit.Test;
 
 public class RearrangingCarsTest {
 
-  /*
-   * In real world the rearrangeCars and pathForEmpty would be diff tests in case String
-   * needs to change
-   */
+
   @Test
   public void testEmpty() {
-    assertEquals("", RearrangingCars.rearrangeCars(null, null));
-    assertEquals("", RearrangingCars.rearrangeCars(null, new int[1]));
-    assertEquals("", RearrangingCars.rearrangeCars(new int[1], null));
-    assertEquals("", RearrangingCars.rearrangeCars(new int[1], new int[2]));
-    assertEquals("", RearrangingCars.rearrangeCars(new int[0], new int[0]));
+    assertEquals("", RearrangingCars.rearrangeCars(null, null).toString());
+    assertEquals("", RearrangingCars.rearrangeCars(null, new int[1]).toString());
+    assertEquals("", RearrangingCars.rearrangeCars(new int[1], null).toString());
+    assertEquals("", RearrangingCars.rearrangeCars(new int[1], new int[2]).toString());
+    assertEquals("", RearrangingCars.rearrangeCars(new int[0], new int[0]).toString());
   }
 
   @Test
@@ -32,8 +26,7 @@ public class RearrangingCarsTest {
             + "move from 0 to 3\n";
     int[] source = {1, 2, 0, 3};
     int[] target = {3, 1, 2, 0};
-    assertEquals(res, RearrangingCars.rearrangeCars(source, target));
-    assertEquals(Arrays.asList(2, 1, 0, 3), RearrangingCars.pathForEmpty(source, target));
+    assertEquals(res, RearrangingCars.rearrangeCars(source, target).toString());
   }
 
   @Test
@@ -45,8 +38,7 @@ public class RearrangingCarsTest {
             + "move from 1 to 2\n";
     int[] source = {1, 2, 0, 3};
     int[] target = {3, 1, 0, 2};
-    assertEquals(res, RearrangingCars.rearrangeCars(source, target));
-    assertEquals(Arrays.asList(2, 0, 3, 1, 2), RearrangingCars.pathForEmpty(source, target));
+    assertEquals(res, RearrangingCars.rearrangeCars(source, target).toString());
   }
 
   @Test
@@ -67,9 +59,7 @@ public class RearrangingCarsTest {
             + "move from 6 to 10\n";
     int[] target = IntStream.rangeClosed(0, 10).map(i -> 10 - i).toArray();
     int[] source = IntStream.rangeClosed(0, 10).toArray();
-    assertEquals(res, RearrangingCars.rearrangeCars(source, target));
-    assertEquals(Arrays.asList(0, 10, 1, 9, 10, 2, 8, 10, 3, 7, 10, 4, 6, 10),
-        RearrangingCars.pathForEmpty(source, target));
+    assertEquals(res, RearrangingCars.rearrangeCars(source, target).toString());
   }
 
   @Test
@@ -87,9 +77,7 @@ public class RearrangingCarsTest {
             + "move from 0 to 10\n";
     int[] source = {1, 2, 3, 4, 5, 0, 6, 7, 8, 9, 10};
     int[] target = IntStream.rangeClosed(0, 10).map(i -> 10 - i).toArray();
-    assertEquals(res, RearrangingCars.rearrangeCars(source, target));
-    assertEquals(Arrays.asList(5, 4, 6, 3, 7, 2, 8, 1, 9, 0, 10),
-        RearrangingCars.pathForEmpty(source, target));
+    assertEquals(res, RearrangingCars.rearrangeCars(source, target).toString());
   }
 
   @Test
@@ -101,8 +89,7 @@ public class RearrangingCarsTest {
             + "move from 3 to 1\n";
     int[] source = {1, 2, 0, 3};
     int[] target = {3, 0, 2, 1};
-    assertEquals(res, RearrangingCars.rearrangeCars(source, target));
-    assertEquals(Arrays.asList(2, 1, 0, 3, 1), RearrangingCars.pathForEmpty(source, target));
+    assertEquals(res, RearrangingCars.rearrangeCars(source, target).toString());
   }
 
   @Test
@@ -120,17 +107,12 @@ public class RearrangingCarsTest {
             + "move from 6 to 1\n";
     int[] source = {1, 2, 0, 3, 5, 6, 7, 8};
     int[] target = {3, 0, 2, 1, 8, 7, 6, 5};
-    assertEquals(res, RearrangingCars.rearrangeCars(source, target));
-    assertThat(RearrangingCars.pathForEmpty(source, target),
-        is(Arrays.asList(2, 1, 0, 3, 1, 4, 7, 1, 5, 6, 1)));
-    assertEquals(Arrays.asList(2, 1, 0, 3, 1, 4, 7, 1, 5, 6, 1),
-        RearrangingCars.pathForEmpty(source, target));
+    assertEquals(res, RearrangingCars.rearrangeCars(source, target).toString());
   }
 
   @Test
   public void testAlreadyCorrect() {
     int[] arr = {1, 2, 0, 3, 5, 6, 7, 8};
-    assertEquals("", RearrangingCars.rearrangeCars(arr, arr));
-    assertEquals(Arrays.asList(2), RearrangingCars.pathForEmpty(arr, arr));
+    assertEquals("", RearrangingCars.rearrangeCars(arr, arr).toString());
   }
 }
